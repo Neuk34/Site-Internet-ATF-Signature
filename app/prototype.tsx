0831cbbac633a29545e231cb239a595ff43f9f92
Nicolas Legrand <nicolas.legrand34@gmail.com>
2026-08-23 23:15:00 +0200

    Use transparent contact button artwork

--- app/prototype.tsx
+++ app/prototype.tsx
@@ -36,7 +36,7 @@ function ContactButton() {
       href={siteConfig.contact.phoneHref}
       aria-label={`Nous contacter au ${siteConfig.contact.phoneDisplay}`}
     >
-      <img src="/media/contact-button.png" alt="" width={1860} height={846} />
+      <img src="/media/contact-button.png" alt="" width={2172} height={724} />
     </a>
   );
 }
